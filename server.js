079be968aef348e10c65ac4737a73daba6e5dad9
ttsssss <sandovaltrent@gmail.com>
2022-02-15 18:42:20 -0700

    3rd commit of login page

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const routes = require("./routes");
-const path = require("path");
-
-const PORT = process.env.PORT || 3001;
-require("dotenv").config();
-const cors = require("cors");
